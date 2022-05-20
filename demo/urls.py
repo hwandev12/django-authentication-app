b3d269bb81b8560c347af4d346c3b3e37f0f6f9a
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-20 17:20:40 +0500

    update url

--- demo/urls.py
+++ demo/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls'))
