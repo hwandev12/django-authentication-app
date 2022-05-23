c5ee23078f3ddf9db37bdf6c8263a032f1fb8058
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-23 16:59:34 +0500

    create custom user model admin

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-admin.site.register(CustomUser)
+
+class CustomUserAdmin(admin.ModelAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ['username', 'email']
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
