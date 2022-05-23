e7890e4a19cbb156388fd6216c4bf09a7fdf2257
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-23 11:56:00 +0500

    login authentication templates

--- core/views.py
+++ core/views.py
@@ -1,1 +1,6 @@
 from django.shortcuts import render
+
+
+# create home page view
+def home(request):
+    return render(request, 'pages/home.html')
--- demo/urls.py
+++ demo/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from core.views import home
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
 
