02984370dcc437950e5b0e7ee0ce042e756f8c74
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-23 17:23:00 +0500

    create sign up url

--- demo/urls.py
+++ demo/urls.py
@@ -2,12 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from core.views import home
+from core.views import *
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('signup/', SignUpView.as_view(), name='signup'),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
 
